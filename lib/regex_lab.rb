219c94fc3c3b8167629069c805e387409220188a
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 03:47:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/[un]\w+ing/)
+  if text.match(/un\w+ing/)
     true 
   else
     false
@@ -15,7 +15,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.match(/\w{5}/)
+  if text.match(/^\w{5}\s/)
     true
   else
     false
