f2333fece8b6eeeab59732576a8ea30a991eea99
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 04:22:53 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+   arr = text.split(" ")
+  arr.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  capitalized=text.scan(/\w+/)[0]
+if (capitalized == capitalized.upcase) && (text.scan(/\./)) 
+  true
+else
+  false
+end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
