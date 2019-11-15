1b634242dcfe8253b8cfbd095fdb55fd42efc761
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 04:06:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if capitalized=text.scan(/\w+/)[0]
+if capitalized=text.scan(/\w+/)[0] && text.scan(//)
   true
 else
   false
