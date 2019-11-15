12cbc9bac80bf46d8b731b793939b4b81fc2e01f
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 03:46:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^[un]|\w+ing/)
+  if text.match(/^[un]\w+ing/)
     true 
   else
     false
