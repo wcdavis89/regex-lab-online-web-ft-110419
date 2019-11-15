33b8da8e3e74003757fcee820cfb8bdae3b51cc1
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 04:01:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,8 +16,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-capitalized=text.scan(/\w+/)[0]
-capitalized << text
+if capitalized=text.scan(/\w+/)[0]
+  true
+else
+  false
+end
+
 end
 
 def valid_phone_number?(phone)
