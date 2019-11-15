a0a4de1b202b60c92500556abbf08841c85f9533
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 03:39:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,16 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if word.match(/^[un]|\w+ing/)
+    true 
+  else
+    false
 end
 
 def words_five_letters_long(text)
