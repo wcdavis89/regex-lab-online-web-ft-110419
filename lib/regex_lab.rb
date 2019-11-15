3e62d9848464ddc600c3289f200d8146ebfd2efa
Whittney Davis <wcdavis89@yahoo.com>
2019-11-15 03:49:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/un\w+ing/)
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-  if text.grep(/\b\w{5}\b/)
+   text.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
